aab4938383442f2e1de5492ff7fa508a4c304c7d
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-09-26 20:42:17 +0900

    #2 MyImage

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/image_test/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/image_test/MainActivity.java
@@ -15,24 +15,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(new MyImage(this));
     }
-    class MyImage extends View {
-        MyImage(Context context){
-            super(context);  //view상속 가능
-        }
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            Bitmap picture = BitmapFactory.decodeResource(getResources(),R.drawable.daramge);
-            float cx=getWidth()/2.0f;
-            float cy=getHeight()/2.0f;
-            float x=(getWidth()-picture.getWidth())/2.0f;
-            float y=(getHeight()-picture.getHeight())/2.0f;
-            //canvas.rotate(180,cx,cy); //회전
-            //canvas.translate(-150,200);//이동
-           // canvas.scale(2,2,cx,cy);//크기변경
-            canvas.skew(0.4f,0.4f);
-            canvas.drawBitmap(picture,x,y,null);
-        }
-    }
 }
---
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/image_test/MyImage.java
@@ -0,0 +1,30 @@
+package kr.hs.emirim.dbwlsdud0407.image_test;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by appcreator08 on 2016. 9. 26..
+ */
+public class MyImage extends View {
+    MyImage(Context context){
+        super(context);  //view상속 가능
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Bitmap picture = BitmapFactory.decodeResource(getResources(),R.drawable.daramge);
+        float cx=getWidth()/2.0f;
+        float cy=getHeight()/2.0f;
+        float x=(getWidth()-picture.getWidth())/2.0f;
+        float y=(getHeight()-picture.getHeight())/2.0f;
+        //canvas.rotate(180,cx,cy); //회전
+        //canvas.translate(-150,200);//이동
+        // canvas.scale(2,2,cx,cy);//크기변경
+        canvas.skew(0.4f,0.4f);
+        canvas.drawBitmap(picture,x,y,null);
+    }
+}
+
