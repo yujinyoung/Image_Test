2d5aaeca5f9324595f450f47b41fff6604d122ca
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-09-27 20:21:46 +0900

    #4 Make Button

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/image_test/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/image_test/MainActivity.java
@@ -4,16 +4,53 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+    MyImage mIng;
+    static final int ORIGINAL=0;
+    static final int ROTATE=1;
+    static final int TRANSLATE=2;
+    static final int SCALE=3;
+    static final int SKEW=4;
+    int choose=ORIGINAL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(new MyImage(getApplicationContext()));
         setContentView(R.layout.activity_main);
+        LinearLayout linear1 = (LinearLayout)findViewById(R.id.linear1);
+        mIng = new MyImage(getApplicationContext());
+        linear1.addView(mIng);
     }
+    public void imageTurn(View v){
+        switch (v.getId()){
+            case R.id.b1 :
+                choose=ROTATE;
+                break;
+            case R.id.b2:
+                choose=TRANSLATE;
+                break;
+            case R.id.b3:
+                choose=SCALE;
+                break;
+            case R.id.b4:
+                choose=SKEW;
+                break;
+        }
+        mIng.setChoose(choose);
+        mIng.invalidate(); //onDraw를 다시 호출하는 방법
 
+    }
 }
--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/image_test/MyImage.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/image_test/MyImage.java
@@ -10,12 +10,21 @@ import android.view.View;
  * Created by appcreator08 on 2016. 9. 26..
  */
 public class MyImage extends View {
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
     MyImage(Context context) {
+
         super(context);  //view상속 가능
+        picture = BitmapFactory.decodeResource(getResources(), R.drawable.daramge);
+    }
+    public void setChoose(int choose){
+        this.choose=choose;
+
+
     }
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.daramge);
+
         float cx = getWidth() / 2.0f;
         float cy = getHeight() / 2.0f;
         float x = (getWidth() - picture.getWidth()) / 2.0f;
@@ -23,7 +32,22 @@ public class MyImage extends View {
         //canvas.rotate(180,cx,cy); //회전
         //canvas.translate(-150,200);//이동
         // canvas.scale(2,2,cx,cy);//크기변경
-        canvas.skew(0.4f, 0.4f);
+        //canvas.skew(0.4f, 0.4f);
+
+        switch (choose){
+            case MainActivity.ROTATE:
+                canvas.rotate(180,cx,cy);
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150,200);
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(2,2,cx,cy);
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f, 0.4f);
+                break;
+        }
         canvas.drawBitmap(picture, x, y, null);
     }
   }
